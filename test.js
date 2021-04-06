6588412b4e1b4bc05165ee83334b09ed7bc46a1c
Grace Okereke <graceokereke22@gmail.com>
2021-04-06 07:42:23 +0100

    changes

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
+const lex = require("./sampleTest.js");
 const readline = require("readline");
-const analyzer = require("./lexer.js");
-
 const getInput = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 getInput.question("Input your code snippets ", function saveInput(codeSnippets) {
-  const setOutput = new analyzer (codeSnippets);
-  console.log(setOutput);
-  getInput.close();
+    const setOutput = lex(codeSnippets);
+    for (var lexeme; lexeme = setOutput.next();) {
+        console.log(lexeme);
+    }
+    getInput.close();
 });
 
 getInput.on("close", function saveInput() {
